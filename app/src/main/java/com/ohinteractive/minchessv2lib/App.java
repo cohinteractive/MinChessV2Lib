26306420b8cfaa643244b336a9669a91d8787a49
Charles Clark <charles@ohinteractive.co.nz>
2025-04-27 08:41:17 +1200

    Update for testing new Board implementation

--- app/src/main/java/com/ohinteractive/minchessv2lib/App.java
+++ app/src/main/java/com/ohinteractive/minchessv2lib/App.java
@@ -1,9 +1,14 @@
 package com.ohinteractive.minchessv2lib;
 
+import com.ohinteractive.minchessv2lib.impl.Board;
+
 public class App {
     
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //String fen =
+        //"k7/8/8/8/8/8/8/K7 w - - 0 1";
+        long[] board = Board.startingPosition();
+        Board.drawText(board);
     }
 
 }
