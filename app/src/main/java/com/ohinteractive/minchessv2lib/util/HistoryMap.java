14fd5741b1b2923019515d0a7429cc4806457e9b
Charles Clark <charles@ohinteractive.co.nz>
2025-05-02 18:57:33 +1200

    Update map for generational updating

--- app/src/main/java/com/ohinteractive/minchessv2lib/util/HistoryMap.java
+++ app/src/main/java/com/ohinteractive/minchessv2lib/util/HistoryMap.java
@@ -8,72 +8,81 @@ public class HistoryMap {
 
     public HistoryMap(int initialCapacity) {
         this.capacity = initialCapacity;
-        this.hashMask = capacity - 1;
-        this.keys = new long[capacity];
-        this.counts = new byte[capacity];
-        this.size = 0;
-    }
-
-    public int increment(long key) {
-        if(this.size >= (this.capacity >>> 1)) resize();
-        int index = (int) key & this.hashMask;
-        while(true) {
-            if(this.keys[index] == EMPTY) {
-                this.keys[index] = key;
-                this.counts[index] = 1;
-                this.size ++;
-                return 1;
-            }
-            if(this.keys[index] == key) return ++ this.counts[index];
-            index = (index + 1) & this.hashMask;
-        }
+        this.hashMask = initialCapacity - 1;
+        this.keys = new long[initialCapacity];
+        this.counts = new byte[initialCapacity];
+        this.generations = new byte[initialCapacity];
+        this.generation = 1;
+        this.threshold = capacity >>> 1;
     }
 
     public void reset() {
-        for(int i = 0; i < this.capacity; i ++) {
-            this.keys[i] = EMPTY;
-            this.counts[i] = 0;
-        }
-        this.size = 0;
-    }
-
-    public void resize() {
-        int newCapacity = capacity << 1;
-        long[] oldKeys = keys;
-        final int oldKeysLength = oldKeys.length;
-        byte[] oldCounts = counts;
-        this.keys = new long[newCapacity];
-        this.counts = new byte[newCapacity];
-        this.capacity = newCapacity;
-        this.hashMask = this.capacity - 1;
-        this.size = 0;
-        for(int i = 0; i < oldKeysLength; i ++) {
-            long key = oldKeys[i];
-            if(key != EMPTY) {
-                int count = oldCounts[i];
-                reinsert(key, count);
-            }
+        this.generation ++;
+        if(this.generation == 0) {
+            for(int i = 0; i < capacity; i ++) this.generations[i] = 0;
+            this.generation = 1;
         }
     }
 
-    private void reinsert(long key, int count) {
+    public int increment(long key) {
         int index = (int) key & this.hashMask;
         while(true) {
-            if(this.keys[index] == EMPTY) {
+            final int g = this.generations[index];
+            final long k = this.keys[index];
+            if(g == this.generation && k == key) return ++ this.counts[index];
+            if(g != this.generation) {
                 this.keys[index] = key;
-                this.counts[index] = (byte) count;
-                size ++;
-                return;
+                this.counts[index] = 1;
+                this.generations[index] = (byte) this.generation;
+                if(-- threshold == 0) resize();
+                return 1;
             }
             index = (index + 1) & this.hashMask;
         }
     }
 
-    private static final long EMPTY = 0L;
+    public void resize() {
+        final int newCapacity = this.capacity << 1;
+        final long[] oldKeys = this.keys;
+        final byte[] oldCounts = this.counts;
+        final byte[] oldGenerations = this.generations;
+
+        this.keys = new long[newCapacity];
+        this.counts = new byte[newCapacity];
+        this.generations = new byte[newCapacity];
+
+        final int newThreshold = newCapacity >>> 1;
+        final int oldCapacity = this.capacity;
+        final int oldGeneration = this.generation;
+
+        this.capacity = newCapacity;
+        this.hashMask = this.capacity - 1;
+        this.threshold = newThreshold;
+        this.generation = 1;
+
+        for(int i = 0; i < oldCapacity; i ++) {
+            if(oldGenerations[i] != oldGeneration) continue;
+            final long key = oldKeys[i];
+            final byte count = oldCounts[i];
+            int index = (int) key & this.hashMask;
+            while(true) {
+                if(this.generations[index] != generation) {
+                    this.keys[index] = key;
+                    this.counts[index] = count;
+                    this.generations[index] = (byte) this.generation;
+                    break;
+                }
+                index = (index + 1) & this.hashMask;
+            }
+        }
+    }
+
     private long[] keys;
     private byte[] counts;
+    private byte[] generations;
+    private int generation;
     private int capacity;
     private int hashMask;
-    private int size;
+    private int threshold;
 
 }
